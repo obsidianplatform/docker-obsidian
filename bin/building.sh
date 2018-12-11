7635be58e51eb92686a88e81c785f2023341a1a0
Lance <lancehall123@gmail.com>
2018-12-11 22:04:54 +0000

    dependencies

--- bin/building.sh
+++ bin/building.sh
@@ -1,8 +1,8 @@
 # Download Obsidian-QT Git Repo and go to the src/ directory
 mkdir ~/github
 cd ~/github
-git clone https://github.com/obsidianplatform/Obsidian-QT.git && sleep 5
-cd Obsidian-Qt/src
+git clone https://github.com/obsidianplatform/Obsidian-QT.git obsidian-qt
+cd obsidian-qt/src
 
 # Build the obsidian executable file with all processors available
 # This will take a *very long time* depending on how many processors
